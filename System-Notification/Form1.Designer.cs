684ae6258abc2cc1172de09f4da0b87f096318d2
Cj <cj.junio@ftpbuilders.local>
2024-01-19 14:57:45 +0800

    Second Commit

--- System-Notification/Form1.Designer.cs
+++ System-Notification/Form1.Designer.cs
@@ -32,43 +32,57 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.showToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ShowToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ExitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.BttnSave = new System.Windows.Forms.Button();
+            this.BttnCancel = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.FirstNameBox = new System.Windows.Forms.TextBox();
+            this.MiddleNameBox = new System.Windows.Forms.TextBox();
+            this.LastNameBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ValidUntilBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
             this.contextMenuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Font = new System.Drawing.Font("Myriad Pro Light", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(676, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(237, 29);
+            this.label1.Size = new System.Drawing.Size(214, 29);
             this.label1.TabIndex = 0;
             this.label1.Text = "System Notification";
             // 
             // contextMenuStrip1
             // 
             this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.showToolStripMenuItem,
-            this.exitToolStripMenuItem});
+            this.ShowToolStripMenuItem,
+            this.ExitToolStripMenuItem});
             this.contextMenuStrip1.Name = "contextMenuStrip1";
             this.contextMenuStrip1.Size = new System.Drawing.Size(104, 48);
             // 
-            // showToolStripMenuItem
+            // ShowToolStripMenuItem
             // 
-            this.showToolStripMenuItem.Name = "showToolStripMenuItem";
-            this.showToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.showToolStripMenuItem.Text = "Show";
-            this.showToolStripMenuItem.Click += new System.EventHandler(this.showToolStripMenuItem_Click);
+            this.ShowToolStripMenuItem.Name = "ShowToolStripMenuItem";
+            this.ShowToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.ShowToolStripMenuItem.Text = "Show";
+            this.ShowToolStripMenuItem.Click += new System.EventHandler(this.ShowToolStripMenuItem_Click);
             // 
-            // exitToolStripMenuItem
+            // ExitToolStripMenuItem
             // 
-            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.exitToolStripMenuItem.Text = "Exit";
-            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            this.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem";
+            this.ExitToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.ExitToolStripMenuItem.Text = "Exit";
+            this.ExitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
             // 
             // notifyIcon1
             // 
@@ -77,18 +91,148 @@
             this.notifyIcon1.Text = "Settings";
             this.notifyIcon1.Visible = true;
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(22, 129);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(644, 381);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // BttnSave
+            // 
+            this.BttnSave.Font = new System.Drawing.Font("Myriad Pro Light", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BttnSave.Location = new System.Drawing.Point(707, 357);
+            this.BttnSave.Name = "BttnSave";
+            this.BttnSave.Size = new System.Drawing.Size(81, 32);
+            this.BttnSave.TabIndex = 2;
+            this.BttnSave.Text = "Save";
+            this.BttnSave.UseVisualStyleBackColor = true;
+            // 
+            // BttnCancel
+            // 
+            this.BttnCancel.Font = new System.Drawing.Font("Myriad Pro Light", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BttnCancel.Location = new System.Drawing.Point(803, 357);
+            this.BttnCancel.Name = "BttnCancel";
+            this.BttnCancel.Size = new System.Drawing.Size(81, 32);
+            this.BttnCancel.TabIndex = 3;
+            this.BttnCancel.Text = "Cancel";
+            this.BttnCancel.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = global::System_Notification.Properties.Resources._12312312_copy1;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.panel1.Location = new System.Drawing.Point(4, -4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(523, 101);
+            this.panel1.TabIndex = 4;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Green;
+            this.panel2.Location = new System.Drawing.Point(-1, 527);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(923, 10);
+            this.panel2.TabIndex = 5;
+            // 
+            // FirstNameBox
+            // 
+            this.FirstNameBox.Location = new System.Drawing.Point(706, 151);
+            this.FirstNameBox.Name = "FirstNameBox";
+            this.FirstNameBox.Size = new System.Drawing.Size(177, 20);
+            this.FirstNameBox.TabIndex = 6;
+            // 
+            // MiddleNameBox
+            // 
+            this.MiddleNameBox.Location = new System.Drawing.Point(706, 195);
+            this.MiddleNameBox.Name = "MiddleNameBox";
+            this.MiddleNameBox.Size = new System.Drawing.Size(177, 20);
+            this.MiddleNameBox.TabIndex = 7;
+            // 
+            // LastNameBox
+            // 
+            this.LastNameBox.Location = new System.Drawing.Point(706, 237);
+            this.LastNameBox.Name = "LastNameBox";
+            this.LastNameBox.Size = new System.Drawing.Size(177, 20);
+            this.LastNameBox.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Myriad Pro Light", 9.749999F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(705, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 16);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "First Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Myriad Pro Light", 9.749999F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(705, 174);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 16);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Middle Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Myriad Pro Light", 9.749999F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(707, 218);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 16);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Last Name";
+            // 
+            // ValidUntilBox
+            // 
+            this.ValidUntilBox.Location = new System.Drawing.Point(706, 279);
+            this.ValidUntilBox.Name = "ValidUntilBox";
+            this.ValidUntilBox.Size = new System.Drawing.Size(177, 20);
+            this.ValidUntilBox.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Myriad Pro Light", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(708, 260);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 16);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Valid Until :";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(921, 536);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.ValidUntilBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.LastNameBox);
+            this.Controls.Add(this.MiddleNameBox);
+            this.Controls.Add(this.FirstNameBox);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.BttnCancel);
+            this.Controls.Add(this.BttnSave);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label1);
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "System";
+            this.Text = "FTP System";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.Move += new System.EventHandler(this.Form1_Move);
             this.contextMenuStrip1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -98,9 +242,22 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem showToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ShowToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ExitToolStripMenuItem;
         private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button BttnSave;
+        private System.Windows.Forms.Button BttnCancel;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox FirstNameBox;
+        private System.Windows.Forms.TextBox MiddleNameBox;
+        private System.Windows.Forms.TextBox LastNameBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox ValidUntilBox;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- System-Notification/Form1.cs
+++ System-Notification/Form1.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 
 namespace System_Notification
 {
+    
     public partial class Form1 : Form
     {
         public Form1()
@@ -17,13 +18,13 @@ namespace System_Notification
             InitializeComponent();
         }
 
-        private void showToolStripMenuItem_Click(object sender, EventArgs e)
+        private void ShowToolStripMenuItem_Click(object sender, EventArgs e)
         {
             this.Show();
             this.WindowState = FormWindowState.Normal;
         }
 
-        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Application.Exit();
         }
@@ -34,8 +35,27 @@ namespace System_Notification
             {
                 this.Hide();
                 notifyIcon1.ShowBalloonTip(1000, "Certificate Notification", " Certificate Expired as of Today, Click this to know more.", ToolTipIcon.Info);
+
+
             }
 
         }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (var dbContext = new EmplyeeCertManagementEntities1())
+            {
+                List<Empolyee_Table> employees = dbContext.Empolyee_Table.ToList();
+                dataGridView1.DataSource = employees;
+            }
+        }
     }
+
 }
+//this.Hide();
+//notifyIcon1.ShowBalloonTip(1000, Certificate Notification", " Certificate Expired as of Today, Click this to know more.", ToolTipIcon.Info);
+
+
+    //this.Hide();
+    //System.Drawing.Icon customIcon = new System.Drawing.Icon("D:\\Users\\cj.junio\\Downloads\\ico.ico");
+    //notifyIcon1.Icon = customIcon;
+    //notifyIcon1.ShowBalloonTip(1000, "Kurth Chester", "Ilan ba docstamp kaylangan sagot ko na kayong lahat .", ToolTipIcon.None);
